a97241047a9653758e5146de4d905d5b4261a6b6
Rizvan2 <rizvangunaev95@gmail.com>
2025-10-31 17:11:43 +0300

    Подправил текст названия комнаты

--- src/main/java/org/example/gametgweb/gameplay/game/init/DataInitializer.java
+++ src/main/java/org/example/gametgweb/gameplay/game/init/DataInitializer.java
@@ -14,9 +14,9 @@ public class DataInitializer {
     public CommandLineRunner initPlayers(PlayerRepository playerRepository, PasswordEncoder passwordEncoder) {
         return args -> {
             if (playerRepository.count() == 0) {
-                PlayerEntity player1 = new PlayerEntity(passwordEncoder.encode("123"), "нуб", null);
-                PlayerEntity player2 = new PlayerEntity(passwordEncoder.encode("123"), "про", null);
-                PlayerEntity player3 = new PlayerEntity(passwordEncoder.encode("123"), "user3", null);
+                PlayerEntity player1 = new PlayerEntity(passwordEncoder.encode("123"), "Нагибатор2017", null);
+                PlayerEntity player2 = new PlayerEntity(passwordEncoder.encode("123"), "Артьом", null);
+                PlayerEntity player3 = new PlayerEntity(passwordEncoder.encode("123"), "Хакер2013", null);
 
                 playerRepository.save(player1);
                 playerRepository.save(player2);
