c928d5927ff69403a52767bd6c40ab2f47c87fa5
Rizvan2 <rizvangunaev95@gmail.com>
2026-01-03 17:23:03 +0300

    Добавил 2 новых эндпоинта: /CreateDuel и /JoinDuel в DuelController

--- src/main/java/org/example/gametgweb/gameplay/game/duel/application/controllers/DuelController.java
+++ src/main/java/org/example/gametgweb/gameplay/game/duel/application/controllers/DuelController.java
@@ -1,19 +1,22 @@
 package org.example.gametgweb.gameplay.game.duel.application.controllers;
 
-import org.example.gametgweb.gameplay.game.duel.shared.PlayerDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.example.gametgweb.gameplay.game.duel.domain.exception.GameAlreadyExistsException;
 import org.example.gametgweb.gameplay.game.duel.infrastructure.webSocket.DuelManager;
-
+import org.example.gametgweb.gameplay.game.duel.shared.PlayerDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Контроллер, отвечающий за запуск и участие в дуэлях между игроками.
  * <p>
  * Обрабатывает запросы от клиента, связанные с созданием или присоединением к дуэли.
  */
 @RestController
+@Slf4j
 public class DuelController {
 
     /** Сервис, управляющий логикой дуэлей и игровыми сессиями */
@@ -37,10 +40,29 @@ public class DuelController {
     @PostMapping("/StartDuel")
     public String duel(@RequestParam String gameCode,
                        @AuthenticationPrincipal PlayerDetails playerDetails) {
-        // Извлекаем ID игрока из объекта аутентификации
-        Long playerId = playerDetails.playerEntity().getId();
-
         // Передаём управление в бизнес-логику дуэлей
-        return duelManager.joinOrCreateGame(gameCode, playerId);
+        return duelManager.joinOrCreateGame(gameCode, getPlayerId(playerDetails));
+    }
+
+    @PostMapping("/CreateDuel")
+    public String createDuel(@RequestParam String gameCode,
+                                        @AuthenticationPrincipal PlayerDetails playerDetails) {
+        try {
+            log.info(gameCode);
+            return duelManager.createGame(gameCode, getPlayerId(playerDetails));
+        } catch (GameAlreadyExistsException e) {
+            return e.getMessage();
+        }
+    }
+
+    @PostMapping("/JoinDuel")
+    public String joinDuel(@RequestParam String gameCode,
+                           @AuthenticationPrincipal PlayerDetails playerDetails) {
+        // Передаём управление в бизнес-логику дуэлей
+        return duelManager.joinGame(gameCode, getPlayerId(playerDetails));
+    }
+
+    private Long getPlayerId(PlayerDetails playerDetails) {
+        return playerDetails.playerEntity().getId();
     }
 }
